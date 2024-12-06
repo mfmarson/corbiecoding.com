c908bac6923220f15904e07628f651422ef21e46
Megan <megan.marson22@gmail.com>
2024-12-05 21:15:16 -0500

    initial commit

---
+++ app/pages/hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="bg-black h-screen ">
+      {/* <h1 className="text-6xl text-white">Welcome to Corbie Coding</h1> */}
+      <h2 className="text-3xl text-white ">
+        Soar above expectations in digital innovation.
+      </h2>
+    </div>
+  );
+}
---
+++ app/pages/main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div >
+      <p className="mb-40">Corbie Coding LLC</p>
+    </div>
+  );
+}
